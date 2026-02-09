c88164c52d4a43af73f6f5320cad99e68a00f454
Rjankit999 <rjankitmaurya@gmail.com>
2026-02-09 22:16:17 +0530

    update

--- factorial.c
+++ factorial.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 int main(){
+    printf("Enter a Nauber: ");
+    int n,fact=1;
+    scanf("%d",&n);
+    for (int i = 2; i <=n; i++)
+    {
+        fact=fact*i;
+    }
+    printf("%d",fact);
+    
     
     return 0;
 }
